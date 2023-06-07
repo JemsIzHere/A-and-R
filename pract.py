ae5982817004d4b96d86b25569338fe22ce0f623
Jems <james.maranion13@gmail.com>
2023-06-07 15:13:01 +0800

    updated index

--- pract.py
+++ pract.py
@@ -1,32 +1,41 @@
-s = str(input())
-str = []
-
-for z in s:
-    str.append(z)
-#isalnum
-#isalpha
-#isdigit
-#islower
-#isupper
-
-arr = []
-a = False
-d = False
-l = False
-s = False
+num1 = [1,3]
+num2 = [2]
+num3 = []
+mid = len(num3)/2
+median = 0
 
 
-for i in str:
-    a = a + i.isalnum()
-    d = i.isalpha()
-    l = i.isdigit()
-    s = i.isupper()
-arr.append(a)
-arr.append(d)
-arr.append(l)
-arr.append(s)
+def merge(arr1,arr2):
+    ans =[]
+    i=j=0
+    while i < len(arr1) and j<len(arr2):
+        if arr1[i]<=arr2[j]:
+            ans.append(arr1[i])
+            i+=1
+        else:
+            ans.append(arr2[j])
+            j+=1
+    ans.extend(arr1[i:])
+    ans.extend(arr2[j:])
 
-for x in arr:
-    print(arr)
+    return ans
 
+num3 = merge(num1,num2)
+
+if len(num3)%2 ==0:
+    median = (num3[mid-1]+num3[mid]/2)
+    print("yes")
+else:
+    median = len(num3)/2
+    print("yes1")
+
+
+print(mid)
+
+print(median)
+#median = 
+
+            
+
+    
 
